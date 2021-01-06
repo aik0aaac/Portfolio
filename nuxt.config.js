df999fbb0c86f01f93072f5064a4ac7e10ff3cc5
SaikaUra <aik0aaac@gmail.com>
2021-01-06 18:05:03 +0900

    ページのtitleを変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,8 +17,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - Portfolio-Aik',
-    title: 'Portfolio-Aik',
+    titleTemplate: "%s - Aik's Portfolio",
+    title: "Aik's Portfolio",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
